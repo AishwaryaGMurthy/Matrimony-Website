cf9013cb9be965575edd972acc1d4938c4004584
AishwaryaGMurthy <aishwarya.murthy@gmail.com>
2017-10-06 12:11:14 -0700

    Add files via upload

---
+++ welcome.php
@@ -0,0 +1,19 @@
+
+<HTML>
+<HEAD>
+</HEAD>
+<body>
+
+Welcome <?php echo $_GET["names"]; ?>
+
+Your phone number is :<?php echo $_GET["phone"]; ?><br>
+
+Your username is: <?php echo $_GET["usernames"]; ?><br>
+
+Your email address is: <?php echo $_GET["mail"]; ?><br>
+
+The scheme you have selected is:<?php echo $_GET["schemes"]; ?><br>
+
+
+</body>
+</html>
